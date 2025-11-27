2e1f211d89e192734f88590faf9614d251e525bb
morerdejlig <76737356+morerdejlig@users.noreply.github.com>
2025-11-27 10:39:59 +0100

    changed powershell scripts

--- Scripts/Phase_2/Fuzzer/actions/Service_Logon.ps1
+++ Scripts/Phase_2/Fuzzer/actions/Service_Logon.ps1
@@ -6,12 +6,9 @@ param(
 # Create service using LocalService account
 sc.exe create $ServiceName binPath= "\"$Executable\"" obj= "NT AUTHORITY\LocalService" type= own
 
-# Start service → triggers 4624 Type 5
 Start-Service $ServiceName
 Start-Sleep -Seconds 2
 
-# Stop service → triggers 4634 Type 5
 Stop-Service $ServiceName -Force
 
-# Remove service
 sc.exe delete $ServiceName
