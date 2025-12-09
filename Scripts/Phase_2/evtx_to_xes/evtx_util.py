d4d1db462d1c86189787c17f6724d0c88a36032c
morerdejlig <76737356+morerdejlig@users.noreply.github.com>
2025-12-09 14:51:47 +0100

    new data

--- Scripts/Phase_2/evtx_to_xes/evtx_util.py
+++ Scripts/Phase_2/evtx_to_xes/evtx_util.py
@@ -5,6 +5,7 @@ import math
 import pm4py
 from pm4py.objects.conversion.log import converter as log_converter
 from pm4py.objects.log.obj import EventLog, Trace
+import os
 
 def _is_nan_value(v):
     if v is None:
@@ -47,20 +48,20 @@ def _parse_event_xml(xml_string):
 
     return event_dict
 
-def evtx_to_csv(evtx_path, csv_out):
+def evtx_list_to_csv(evtx_paths, csv_out):
     rows = []
 
-    with Evtx(evtx_path) as log:
-        for record in log.records():
-            xml = record.xml()
-            event_dict = _parse_event_xml(xml)
-            rows.append(event_dict)
+    for evtx_path in evtx_paths:
+        with Evtx(evtx_path) as log:
+            for record in log.records():
+                xml = record.xml()
+                event_dict = _parse_event_xml(xml)
+                event_dict["__source_evtx"] = os.path.basename(evtx_path)
+                rows.append(event_dict)
 
     df = pd.DataFrame(rows)
     df.to_csv(csv_out, index=False)
-    print(f"CSV written to {csv_out}")
-
-
+    print(f"Combined CSV written to {csv_out} from {len(evtx_paths)} EVTX files")
 
 def csv_to_xes(csv_path, xes_out):
     df = pd.read_csv(csv_path)
@@ -314,11 +315,17 @@ def drop_trailing(event_log):
     return new_log
 
 if __name__ == "__main__":
-    EVTX_FILE = "/Users/emilpontoppidanrasmussen/Desktop/master/MasterRepo/Personal/1000ScriptsBackup.evtx"
+    EVTX_FILE1 = "/Users/emilpontoppidanrasmussen/Desktop/master/MasterRepo/Scripts/Phase_2/evtx_files/300Scripts1_backup.evtx"
+    EVTX_FILE2 = "/Users/emilpontoppidanrasmussen/Desktop/master/MasterRepo/Scripts/Phase_2/evtx_files/300Scripts2_backup.evtx"
+    EVTX_FILE3 = "/Users/emilpontoppidanrasmussen/Desktop/master/MasterRepo/Scripts/Phase_2/evtx_to_xes/evtx_files/300Scripts3_backup.evtx"
+
+    evtx_files = [EVTX_FILE1, EVTX_FILE2, EVTX_FILE3]
+
     CSV_OUT = "/Users/emilpontoppidanrasmussen/Desktop/master/MasterRepo/GeneratedFiles/evtx_csv/evtx_csv_1000.csv"
     XES_OUT = "/Users/emilpontoppidanrasmussen/Desktop/master/MasterRepo/GeneratedFiles/csv_xes/csv_xes_time_window.xes"
 
-    #evtx_to_csv(EVTX_FILE, CSV_OUT)
+    evtx_list_to_csv(evtx_files, CSV_OUT)
     windows = get_fuzz_time_windows_from_csv(CSV_OUT)
     csv_to_xes_time_windows(CSV_OUT, XES_OUT, windows)
-    print("Generated XES from EVTX")
+
+    print("Generated XES from combined EVTX files")
