dd9ba14a85c30c62bf48235890ac112ae6b71231
EmilLM <lomo0841@gmail.com>
2026-01-07 17:08:14 +0100

    add probabilities to actions

--- Scripts/Phase_2/Fuzzer/action_generator.py
+++ Scripts/Phase_2/Fuzzer/action_generator.py
@@ -1,44 +1,31 @@
 import random
+from enum import Enum
 
 
 # TERMINAL SYMBOLS
 
-SUCCESSFUL_MAIN_LOGONS = [
-    "Interactive_Logon",
-    #"Network_Logon"
-]
+class MainLogon(str, Enum):
+    INTERACTIVE = "Interactive_Logon"
 
-FAILED_LOGON = "Failed_Logon"
 
-SUBLOGONS = [
-    #"Batch_Logon",
-    #"Service_Logon", TODO: fix and add back in
-    #"RunAs_Logon",
-    "Network_Logon"
-]
+class SubLogon(str, Enum):
+    NETWORK = "Network_Logon"
 
-OBJECT_OPS = [
-    "Create_Object",
-    "Delete_Object",
-    "Modify_Object"
-]
 
-REGISTRY_OPS = [
-    #"Create_Registry",
-    #"Delete_Registry",
-    #"Modify_Registry",
-    #"Modify_CommonStartup_Registry"
-]
+class ObjectOp(str, Enum):
+    CREATE = "Create_Object"
+    DELETE = "Delete_Object"
+    MODIFY = "Modify_Object"
 
-PROCESS_OPS_GENERIC = ["Start_Process"]
-PROCESS_OPS_CMD = ["Start_CMD_Process"]
 
-LOCK_UNLOCK_PAIR = ["Lock_Workstation", "Unlock_Workstation"]
+class ProcessOp(str, Enum):
+    GENERIC = "Start_Process"
+    CMD = "Start_CMD_Process"
 
-INTERACTIVE_LOGOFF = "Interactive_Logoff"
-NETWORK_LOGOFF = "Network_Logoff"
 
-SUBLOGOFF = ["Sub_Logoff"]
+class Logoff(str, Enum):
+    INTERACTIVE = "Interactive_Logoff"
+    NETWORK = "Network_Logoff"
 
 
 # PRODUCTION RULES
@@ -51,26 +38,16 @@ def generate_SESSION():
 
 def generate_MAIN_LOGON():
     events = []
-
-    # FAILED_LOGON
-    if random.random() < 0.25:
-        failures = random.randint(1, 3)
-        events.extend([FAILED_LOGON] * failures)
-
-    # SUCCESSFUL_MAIN_LOGON
-    main_logon_choice = random.choice(SUCCESSFUL_MAIN_LOGONS)
-    events.append(main_logon_choice)
-    if main_logon_choice == "Interactive_Logon":
-        events.extend(generate_ACTIONS())
-        events.append(INTERACTIVE_LOGOFF)
-    elif main_logon_choice == "Network_Logon":
-        events.append(NETWORK_LOGOFF)
+    
+    events.append(MainLogon.INTERACTIVE.value)
+    events.extend(generate_ACTIONS())
+    events.append(Logoff.INTERACTIVE.value)
 
     return events
 
+
 def generate_ACTIONS():
     events = []
-
     num_actions = random.randint(0, 4)
 
     for _ in range(num_actions):
@@ -80,71 +57,52 @@ def generate_ACTIONS():
 
 
 def generate_ACTION():
-    ACTION = [
-        "SUBLOGON",
-        "OBJECT_OP",
-        #"REGISTRY_OP",
-        "PROCESS_OP",
-        #"LOCK_UNLOCK" TODO: Remove?
-    ]
-    choice = random.choice(ACTION)
+    r = random.random()
 
-    if choice == "SUBLOGON":
+    # 50%
+    if r < 0.5:
         return generate_SUBLOGON()
-
-    elif choice == "OBJECT_OP":
+    # 30%
+    elif r < 0.8:
         return generate_OBJECT_OP()
-
-    elif choice == "REGISTRY_OP":
-        return generate_REGISTRY_OP()
-
-    elif choice == "PROCESS_OP":
+    # 20%
+    else:
         return generate_PROCESS_OP()
-
-    elif choice == "LOCK_UNLOCK":
-        return generate_LOCK_UNLOCK()
-
-    return []
 
 
 def generate_SUBLOGON():
     events = []
 
-    sublogon_choice = random.choice(SUBLOGONS)
-    events.append(sublogon_choice)
+    events.append(SubLogon.NETWORK.value)
+    events.append(Logoff.NETWORK.value)
 
-    if sublogon_choice == "Network_Logon":
-        events.append(NETWORK_LOGOFF)
-
-    #if sublogin_choice == "RunAs_Logon":
-        #events.extend(generate_ACTIONS())
-        #events.extend(generate_SUBLOGOFF())  # TODO: No sublogoff command - remove? And what do we do with actions?
     return events
-
-def generate_SUBLOGOFF():
-    return [random.choice(SUBLOGOFF)]
 
 
 def generate_OBJECT_OP():
-    return [random.choice(OBJECT_OPS)]
+    r = random.random()
 
-
-def generate_REGISTRY_OP():
-    return [random.choice(REGISTRY_OPS)]
+    # 60%
+    if r < 0.6:
+        return [ObjectOp.MODIFY.value]
+    # 30%
+    elif r < 0.9:
+        return [ObjectOp.CREATE.value] 
+    # 10%
+    else:
+        return [ObjectOp.DELETE.value]
 
 
 def generate_PROCESS_OP():
-    if random.random() < 0.8:
-        return PROCESS_OPS_GENERIC[:] 
+    # 70% 
+    if random.random() < 0.7:
+        return [ProcessOp.GENERIC.value]
+    # 30%
     else:
-        return PROCESS_OPS_CMD[:]      
+        return [ProcessOp.CMD.value]
 
 
-def generate_LOCK_UNLOCK():
-    return LOCK_UNLOCK_PAIR[:]
-
-
-#TEST
+# TEST
 if __name__ == "__main__":
     seq = generate_SESSION()
     for e in seq:
